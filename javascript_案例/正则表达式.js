8f446df2c662cd289628bde83bd0fe03de3679c7
like115 <258861342@qq.com>
2023-07-07 16:58:55 +0800

    sd

--- javascript_案例/正则表达式.js
+++ javascript_案例/正则表达式.js
@@ -1,7 +1,21 @@
-var str = 'abc  def jhi jklmnopqrstuvwxyz'  ;
-var pat = /\s/g ;
-// console.log(str.match(pat));
+var str = '10056元'  ;
+var pat = /\d+(?=元)/g ;
+//  console.log(str.match(pat));
+//  console.log(pat.test(str));
+
+var str1 = '138fgfgd';
+var pat1 = /(?:138)\w+(gd)/ ;
+
+// str1.match(pat1);
+
+// console.log(RegExp.$1);   //打印分组数据
+
+var str2 = 'ahhhbcdkkkbclll' ;
+var pat2 = /bc/g ;
+
+console.log(pat2.exec(str2));
+
 // console.log(str.replace(pat,''));
 
+// console.log('as'.includes('j'));   es6的新函数
 
-console.log('as'.includes('j'));
