e806236e7463015394a528a07d45d98b64d66b6c
like115 <258861342@qq.com>
2023-08-17 10:44:26 +0800

    sd

--- vue_study/vite/myapp02/components/Child.vue
+++ vue_study/vite/myapp02/components/Child.vue
@@ -1,7 +1,45 @@
 <template>
    <div>
-       child
+       child--{{ mytitle }}{{myname}}
+      <p>
+        {{myleft}}
+      </p> 
+      <button v-show="right">按钮</button>
+      <button @click="handleclick">子传父--通过自定义事件</button>
    </div>
 </template>
 
+<script>
+export default{
+    props:{
+     'mytitle': String,
+     myname:{
+        required:true,             //此字段是必传值            
+        validator(value){
+            return ["aa","bb","cc"].includes(value)    //检查属性取值范围
+        }
+    },
+    'myleft':{
+        type:[String,Number],
+        default:"kk"
+    },
+    'right': Boolean
+
+    },
+    data(){
+        return{
+           datalist:[1,2,3]
+        }
+    },
+    methods:{
+// 通过触发执行父节点的自定义事件event,把子节点的数据传递给父节点 
+        handleclick(){
+            this.$emit('event',this.datalist)  
+        }
+    }
+
+}
+
+
+</script>
 
---
+++ vue_study/vite/myapp02/src/App02.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+     <Child mytitle="父属性" :myname="name" :myleft="left"  :right="aright"
+      @event="handleEvent" >
+
+     </Child>
+
+  </div>
+
+</template>
+
+
+<script>
+import Child from "../components/Child.vue";
+export default{
+   components:{
+      Child,
+   },
+   data(){
+      return{
+        name:"aa",
+        left:12,
+        aright:true
+
+      }
+
+   },
+   methods:{
+      handleEvent(data){
+         console.log("自定义事件",data);
+      }
+   }
+}
+
+</script>
