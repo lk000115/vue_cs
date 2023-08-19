5dea8b5d6d950f72ff461724eeac2e51a588e176
lk0115 <258861342@qq.com>
2023-08-19 20:36:11 +0800

    bf

---
+++ vue_study/vite/myapp02/插槽/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+       <Nowplay/>
+    </div>
+</template>
+
+<script>
+import Nowplay from "./Nowplay.vue";
+import axios  from '../../myapp/node_modules/axios/lib/axios'
+
+export default {
+   components:{
+      Nowplay,
+   },
+   data(){
+    return{
+       datalist:[] 
+    }
+   },
+   methods:{
+    
+   },
+   mounted(){
+      axios.get("test_my.json").then(res=>{
+           console.log(res.data.data.hot);
+      })
+    //   fetch("test_my.json").then(res=>res.json())
+    //   .then(res=>{
+    //     console.log(res.data.hot);
+    //   }) 
+   }
+}
+
+</script>
+
+
