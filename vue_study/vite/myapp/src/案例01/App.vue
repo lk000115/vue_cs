d18985329e745872268df431f1d421b7ae6c1b2a
lk0115 <258861342@qq.com>
2023-08-22 22:36:18 +0800

    bf

---
+++ vue_study/vite/myapp/src/案例01/App.vue
@@ -0,0 +1,25 @@
+<template>
+   <div>
+       app----
+      <Child  @event="handleEvent">
+
+      </Child> 
+   </div>
+</template>
+
+<script>
+
+import Child from './Child.vue'
+export default{
+   components:{
+      Child
+   },
+   methods:{
+     handleEvent(data){
+        console.log(data);
+     }
+   }
+
+}
+
+</script>
---
+++ vue_study/vite/myapp/src/案例01/Child.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        child
+        <button @click="handleclick">子节点按钮</button>
+    </div>   
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+          childtitle:"child---111"  
+        }
+    },
+    methods:{
+       handleclick(){
+          this.$emit("event",this.childtitle)
+       }
+
+    }
+
+}
+
+
+</script>
+
+<style scoped>
+
+div{
+    background:yellow;
+}
+
+
+</style>
+
