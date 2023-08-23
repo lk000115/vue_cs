7565cb107b77c248a527fa717d11bb319face69c
lk0115 <258861342@qq.com>
2023-08-23 21:52:40 +0800

    bf

---
+++ vue_study/vite/myapp02/响应式编程/App-属性.vue
@@ -0,0 +1,37 @@
+<!-- 
+   子传父，父传子在响应式编程中的写法
+
+ -->
+
+<template>
+    <Child title="aa" @event="handleck">
+
+    </Child>
+</template>
+
+
+
+<script>
+import Child from "./Child.vue";
+export default {
+    components: {
+        Child,
+    },
+    setup() {
+        function handleck(v) {
+            console.log("子传父的函数", v);
+        }
+
+        return {
+            handleck
+        }
+
+    }
+
+
+
+
+}
+
+
+</script>
---
+++ vue_study/vite/myapp02/响应式编程/Child.vue
@@ -0,0 +1,22 @@
+<template>
+         <div>
+             child
+         </div>
+
+</template>
+
+
+<script>
+export default{
+    props:['title'],
+    setup({title},{emit}){
+        console.log();
+        emit('event',"来自孩子的问候")
+     
+
+    }
+   
+
+}
+
+</script>
