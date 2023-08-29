9af40d40565edfe9457fea6e2d4ce7786dc00c77
lk0115 <258861342@qq.com>
2023-08-29 22:48:41 +0800

    bf

--- vue_study/vite/myapp/vuex/store/index.js
+++ vue_study/vite/myapp/vuex/store/index.js
@@ -3,12 +3,14 @@
 2 缓存部分异步数据,减少后端服务的访问
  */
 import {createStore} from 'vuex'
+import axios from 'axios'
 
 const store = createStore({
 
     state(){
         return{
-           isTabbarShow:true
+           isTabbarShow:true,
+           cinemalist:[]
         }  
     },
     //增加状态修改的监视
@@ -18,8 +20,30 @@ const store = createStore({
         },
         hideTabbar(state){
             state.isTabbarShow = false
+        },
+        // changeTabbar(state,payload){
+        //     state.isTabbarShow = payload
+        // },
+        changeCinemaList(state,payload){
+             state.cinemalist = payload
+        }
+    },
+    actions:{
+        getCinemaList(store){
+            // console.log("ajax");
+            axios({
+                url:"https://m.maizuo.com/gateway?cityId=110100&ticketFlag=1&k=699006",
+                headers:{
+                    'X-Client-Info':'{"a":"3000","ch":"1002","v":"5.2.1","e":"16933172852908118061154306","bc":"110100"}',
+                    'X-Host':'mall.film-ticket.cinema.list'
+                }
+            }).then(res=>{
+                // console.log(res.data.data.cinemas);
+                store.commit("changeCinemaList",res.data.data.cinemas)
+            })
         }
     }
+
 
 })
 
--- vue_study/vite/myapp/vuex/views/Cinemas.vue
+++ vue_study/vite/myapp/vuex/views/Cinemas.vue
@@ -1,7 +1,31 @@
 <template>
 
 <div>
-    cinamas
+    <ul>
+        <li v-for="data in $store.state.cinemalist" :key="data.cinemaid">
+             {{ data.name }}
+        </li>
+    </ul>
 </div>
 
 </template>
+
+<script>
+export default{
+    mounted(){
+        if(this.$store.state.cinemalist.length===0){
+             this.$store.dispatch("getCinemaList") 
+        }else{
+            console.log(this.$store.state.cinemalist);
+        }
+
+    }
+     
+
+
+}
+
+
+
+
+</script>
